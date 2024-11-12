04ca6911a7e9ce57f597b7f01e77bef7831ba5c1
Johannes Portzehl <johannes.portzehl@sogeti.com>
2024-11-12 13:27:58 +0100

    Get and Set

--- Threads_old/Prozess.java
+++ Threads_old/Prozess.java
@@ -3,6 +3,12 @@ package Threads_old;
 public class Prozess implements Runnable {
 	int name;
 
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name=name;
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -10,7 +16,7 @@ public class Prozess implements Runnable {
 		for(i=0;i<=10;i++) {
 			//System.out.println("Thread "+name+" zählt "+i);
 		}
-		System.out.println("Thread "+name+" ist fertig.");
+		System.out.println("Thread "+getName()+" ist fertig.");
 	}
 	
 
--- Threads_old/dlg_Threads.java
+++ Threads_old/dlg_Threads.java
@@ -1,5 +1,4 @@
 package Threads_old;
-import Threads_old.Prozess;
 
 public class dlg_Threads {
 	public static void main (String[]args)
@@ -8,8 +7,8 @@ public class dlg_Threads {
 		i = 0;
 		anz = 10;
 		while (i <= anz) {
-			Threads_old.Prozess p = new Threads_old.Prozess();
-			p.name = i;
+			Prozess p = new Prozess();
+			p.setName(i);
 			Thread t = new Thread(p);
 			t.start();
 			i++;
