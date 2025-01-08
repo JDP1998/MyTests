ae1c004d1d3225b447be9cebe22f98e0fa12b901
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-01-08 14:52:57 +0100

    Add Binary Search

---
+++ src/Search/Binary_Search.java
@@ -0,0 +1,53 @@
+package Search;
+
+import java.util.Arrays;
+
+public class Binary_Search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] numbers = new int [999999];
+		int begin = 0,end,number, rounds=1,middle=0,counter=0;
+		boolean is_gefunden=false;
+		
+		//Declare number
+		number=(int)(Math.random()*999999)+1;
+		System.out.println(number);
+		System.out.println("\r\n");
+		
+		//Declare end
+		end=numbers.length;
+		
+		//Fill Array
+		for(counter=0;counter<=numbers.length-1;counter++) {
+			numbers[counter]=counter+1;
+		}
+		
+		//Sort Array
+		Arrays.sort(numbers);
+		
+		while(is_gefunden==false) {
+			middle=end-begin;
+			middle=middle/2;
+			middle=middle+begin;
+			if(numbers[middle]==number) {
+				is_gefunden=true;
+			}
+			else {
+				if(numbers[middle]>number) {
+					end=middle;
+				}
+				else {
+					begin=middle;
+				}
+			}
+			System.out.println("Anfang "+begin);
+			System.out.println("Ende "+end);
+			System.out.println("\r\n");
+			rounds++;
+		}
+		System.out.println("The number "+number+" was found. It took "+rounds+" turns to find it!");
+
+	}
+
+}
