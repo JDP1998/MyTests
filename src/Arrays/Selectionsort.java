40021825425828ef743d9d4b005939ce3afc5d41
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-01-14 09:50:14 +0100

    Add Selection Sort

--- src/Arrays/Selectionsort.java
+++ src/Arrays/Selectionsort.java
@@ -1,32 +1,35 @@
 package Arrays;
 
-public class Insertsort {
+public class Selectionsort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] numbers = new int [100000];
-		int k,i,min,counter=0;
+		int k = 0,i,min = 0,counter=0;
 		long start, end, time;
 		
 		start=System.currentTimeMillis();
+		
 		for(counter=0;counter<=numbers.length-1;counter++) {
 			numbers [counter] = (int)(Math.random()*100000)+1; 
 		}
+		
 		counter=0;
 		while(counter<=numbers.length-1) {
 			min=numbers[counter];
 			i=counter+1;
-			for(i=0;i<=numbers.length-1;i++) {
-				if(min>=numbers[i]) {
+			while(i<=numbers.length-1) {
+				if(min<=numbers[i]) {
 					
 				}
 				else {
-					k=numbers[i];
-					numbers[i]=numbers[counter];
-					numbers[counter]=k;
-					min=k;
+					min=numbers[i];
+					k=i;
 				}
+				i++;
 			}
+			numbers[k]=numbers[counter];
+			numbers[counter]=min;
 			counter++;
 		}
 		for(Integer l: numbers) {
