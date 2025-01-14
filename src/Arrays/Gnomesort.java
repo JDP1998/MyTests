eb59265e8b8995a2beacda96eb70e65fc1f3991d
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-01-14 15:33:34 +0100

    Add Gnomesort

---
+++ src/Arrays/Gnomesort.java
@@ -0,0 +1,57 @@
+package Arrays;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Gnomesort {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int [] numbers = new int [10000];
+		int k,gnome=0,counter=0;
+		long start, end, time;
+		
+		start=System.currentTimeMillis();
+		
+		File f = new File("C:\\Users\\jportzeh\\Desktop\\Java Dateien\\Numbers.txt");
+		if(f.exists()==false) {
+			f.createNewFile();
+		}
+		
+		BufferedWriter bWriter = new BufferedWriter(new FileWriter(f,false));
+		
+		for(counter=0;counter<=numbers.length-1;counter++) {
+			numbers [counter] = (int)(Math.random()*10000)+1; 
+			bWriter.write(String.valueOf(numbers[counter]));
+			bWriter.write("\r\n");
+		}
+		bWriter.close();
+		
+		while(gnome<=numbers.length-2) {
+			if(numbers[gnome]>numbers[gnome+1]) {
+				k=numbers[gnome+1];
+				numbers[gnome+1]=numbers[gnome];
+				numbers[gnome]=k;
+				if(gnome>0) {
+					gnome--;
+				}
+				else {
+					gnome++;
+				}
+			}
+			else {
+				gnome++;
+			}
+		}
+		for(Integer l: numbers) {
+			System.out.println(l);
+		}
+		end=System.currentTimeMillis();
+		time=end-start;
+		System.out.println("Es hat "+time+" Millisekunden gedauert!");
+
+	}
+
+}
