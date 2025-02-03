a56946d2aebf63fc6f0efd2083b450fe922f4c2d
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-02-03 15:14:37 +0100

    Add Selenium

---
+++ Selenium/Random_Artikel.java
@@ -0,0 +1,28 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Random_Artikel {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		int i=0;
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\jportzeh\\Downloads\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		driver.get("http://de.spongepedia.org/index.php/Hauptseite");
+		Thread.sleep(1000);
+		while(i<=9) {
+			driver.findElement(By.xpath("//*[@id=\"n-randompage\"]/a")).click();
+			Thread.sleep(1000);
+			i++;
+		}
+		System.out.println(driver.findElement(By.xpath("/html/body/div[3]/div/h1/span")).getText());
+		driver.quit();
+	}
+
+}
