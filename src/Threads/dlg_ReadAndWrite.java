965d2c6ed00ccd53895f99b82d032d399292ade9
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-02-03 13:51:23 +0100

    Optimize ReadAndWrite

--- src/Threads/dlg_ReadAndWrite.java
+++ src/Threads/dlg_ReadAndWrite.java
@@ -1,20 +1,24 @@
 package Threads;
 
-
+import java.util.ArrayList;
 
 public class dlg_ReadAndWrite {
 	public static int number=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList <Thread> lst_Threads = new ArrayList<>();
 		int i=1,anz=10;
 		while(i<=anz) {
 			Reader r = new Reader();
 			Writer w = new Writer();
 			Thread read_thread = new Thread(r);
 			Thread write_thread = new Thread(w);
-			read_thread.start();
-			write_thread.start();
+			lst_Threads.add(read_thread);
+			lst_Threads.add(write_thread);
 			i++;
+		}
+		for(Thread t: lst_Threads) {
+			t.start();
 		}
 		System.out.println("Number "+number);
 
