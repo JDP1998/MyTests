835d35ba6826dc9aeff8f9da43fad3c1b022b7bb
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-05-27 14:25:31 +0200

    Fix Errors

--- src/Geheimwort/dlg_Geheimwort.java
+++ src/Geheimwort/dlg_Geheimwort.java
@@ -7,23 +7,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import Entfernung.Stadt;
-
 public class dlg_Geheimwort {
 	public static ArrayList <Country> lst_Countries = new ArrayList();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		boolean is_in_list=false,play=true;
+		boolean is_in_list=false,play=true,solved=false;
 		int round=1,result = 0,random;
 		Scanner s = new Scanner (System.in);
+		Scanner s2 = new Scanner(System.in);
 		String guess,solution;
 		fill_list();
 		random=(int)(Math.random()*lst_Countries.size());
 		solution=lst_Countries.get(random).name;
-		while(play==true) {
+		random++;
+		while(round <= 10 && solved == false) {
 			result=-1;
 			is_in_list=false;
-			System.out.println("Bitte gib ein Land ein!");
+			System.out.println("Runde "+round+" von 10! Bitte gib ein Land ein!");
 			guess=s.nextLine();
 			for(Country c : lst_Countries) {
 				if(guess.equals(c.name)) {
@@ -40,22 +40,31 @@ public class dlg_Geheimwort {
 			if(result==0) {
 				System.out.println("\r\n");
 				System.out.println("Du hast das Geheimwort gelöst. Es war "+solution+". Du hast "+round+" Runden gebraucht");
+				solved=true;
 			}
 			else {
 				if(is_in_list==false) {
-					System.out.println("\r\n");
-					System.out.println("Das Land existiert nicht versuch es nochmal!");
-					System.out.println("\r\n");
+					if(round<10) {
+						System.out.println("\r\n");
+						System.out.println("Das Land existiert nicht versuch es nochmal!");
+						System.out.println("\r\n");
+					}
 				}
 				else {
-					System.out.println("\r\n");
-					System.out.println("Du bist "+result+" von der Lösung entfernt");
-					System.out.println("\r\n");
-					System.out.println("Willst du weiter spielen?");
-					play=s.nextBoolean();
-					System.out.println("\r\n");
+					if(round<10) {
+						System.out.println("\r\n");
+						System.out.println("Du bist " + result + " von der Lösung entfernt");
+						System.out.println("\r\n");
+					}
 				}
 			}
+			round++;
+		}
+		if(solved==true) {
+		}
+		else {
+			System.out.println("\r\n");
+			System.out.println("Du hast es leider nicht geschafft. Die Lösung war "+solution);
 		}
 
 	}
@@ -74,7 +83,7 @@ public class dlg_Geheimwort {
 			c.name=line;
 			c.number=counter;
 			lst_Countries.add(c);
-			count)er++;
+			counter++;
 		}
 		bReader.close();
 	}
