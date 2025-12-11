71e93e721198411b809a6a4d72a7aba1851ecfb1
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-12-11 12:24:33 +0100

    Update Threads

--- Threads_old/Prozess.java
+++ Threads_old/Prozess.java
@@ -1,13 +1,27 @@
-package Threads_old;
+package Threads;
 
 public class Prozess implements Runnable {
-	int name;
+	private int position,wins;
+	private String name;
 
-	public int getName() {
+	
+	public void setWins(int wins) {
+		this.wins=wins;
+	}
+	public int getWins() {
+		return wins;
+	}
+	public String getName() {
 		return name;
 	}
-	public void setName(int name) {
+	public void setName(String name) {
 		this.name=name;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position=position;
 	}
 	@Override
 	public void run() {
@@ -15,7 +29,6 @@ public class Prozess implements Runnable {
 		int i=0,random;
 		random=(int)(Math.random()*10000);
 		for(i=0;i<=10;i++) {
-			//System.out.println("Thread "+name+" zählt "+i);
 			try {
 				Thread.sleep(random);
 			} catch (InterruptedException e) {
@@ -23,7 +36,12 @@ public class Prozess implements Runnable {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("Thread "+getName()+" ist fertig.");
+		System.out.println(getName()+" ist fertig!");
+		setPosition(dlg_Threads.position);
+		dlg_Threads.position++;
+		if(getPosition()==1) {
+			dlg_Threads.winner=getName();
+		}
 	}
 	
 
